4e3ec398b070fde7740291cf18c7d1ce49aacc6b
Adam Lancaster <lancaster28@hotmail.com>
2016-02-22 18:05:30 +0000

    Change counter example from the speclj website

---
+++ spec/change_counter/core_spec.clj
@@ -0,0 +1,43 @@
+(ns change-counter.core-spec
+  (:require [speclj.core :refer :all]
+            [change-counter.core :refer :all]))
+
+(describe "change-counter.core"
+  (around [it]
+    (with-out-str (it)))
+
+  (it "tests the input of the prompt"
+    (should= "13"
+      (with-in-str "13"
+        (prompt "Enter the amount of pennies"))))
+
+  (it "tests the output of a prompt"
+    (should= "Where is my money?\n"
+      (with-out-str (with-in-str "10"
+        (prompt "Where is my money?")))))
+  
+  (it "gets a valid number"
+      (with-redefs [prompt (fn [& _] "11")]
+        (should-not-throw (get-coin-amount "quarters"))
+        (should= 11 (get-coin-amount "pennies"))
+        (should-not= "11" (get-coin-amount "dimes"))
+        (if (not= 11 (get-coin-amount "quarters"))
+          (should-fail "issues getting the correct coin amount")))
+      (with-redefs [prompt (fn [& _] "$5")]
+        (should-throw Error "Not a valid amount"
+          (get-coin-amount "silver dollars"))))
+  
+  (it "returns how much money you have in pennies"
+    (with-redefs[get-coin-amount (fn [& _]2)]
+    (should= 382 (count-change))))
+    
+    (with dollar-format (java.text.DecimalFormat. "$0.00"))
+    
+    (it "converts your change into dollars"
+        (should= "$11.34"
+          (.format @dollar-format
+            (convert-to-dollars 1134.25634456463))))
+)
+
+(run-specs)
+
